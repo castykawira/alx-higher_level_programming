d8e8a901b1f39a89c55d08b84c2ffc391515f220
castykawira <murithicasty@gmail.com>
2023-09-12 10:11:21 -0700

    question 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a function to read and print the content of a text file (UTF-8) to stdout"""
+
+
+def read_file(filename=""):
+    """Read and print the content of a text file to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
