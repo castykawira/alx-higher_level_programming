5e425c4e9c03979d5af8f560e41bdc169644d94a
castykawira <murithicasty@gmail.com>
2023-10-17 05:59:44 -0700

    question 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Task: Print the full description of the table first_table in the specified MySQL database
+-- SQL Query: Print the full description without using DESCRIBE or EXPLAIN
+
+SHOW CREATE TABLE first_table;
