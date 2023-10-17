bd676f961a3b8e17767d1d886b6d80466001c956
castykawira <murithicasty@gmail.com>
2023-10-17 06:44:09 -0700

    question 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Task: Compute the score average of all records in the table second_table in the specified MySQL database
+-- SQL Query: Compute the score average
+
+SELECT AVG(score) AS average FROM second_table;
