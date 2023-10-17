715aa08711c2f91c4b844c172b76c425f86525e4
castykawira <murithicasty@gmail.com>
2023-10-17 06:41:58 -0700

    question 13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Task: Remove records with a score <= 5 from the table second_table in the specified MySQL database
+-- SQL Query: Remove records with score <= 5
+
+DELETE FROM second_table WHERE score <= 5;
