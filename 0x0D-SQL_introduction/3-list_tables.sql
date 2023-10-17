6411ca406e06e232db515bf6b3b51cfa2392387e
castykawira <murithicasty@gmail.com>
2023-10-17 05:48:03 -0700

    question 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,4 @@
-#!/bin/bash
+-- Task: List all tables in a specific MySQL database
+-- MySQL command to list tables in the specified database
 
-# Task: List all tables in a specific MySQL database
-
-# MySQL command to list tables in the specified database
-mysql -e "SHOW TABLES;"
+SHOW TABLES;
