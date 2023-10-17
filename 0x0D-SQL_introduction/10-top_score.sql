fc3dadc875025b2072af1f0f743efad92d0a37ab
castykawira <murithicasty@gmail.com>
2023-10-17 06:36:10 -0700

    question 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Task: List all records of the table second_table in the specified MySQL database
+-- SQL Query: List all records ordered by score
+
+
+SELECT score, name FROM second_table ORDER BY score DESC;
