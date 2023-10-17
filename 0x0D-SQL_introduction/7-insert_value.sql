8e939ee67a4a895812d7f53905af0109fe7fe6e2
castykawira <murithicasty@gmail.com>
2023-10-17 06:22:59 -0700

    question 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Task: Insert a new row into the table first_table in the specified MySQL database
+-- SQL Query: Insert a new row
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
