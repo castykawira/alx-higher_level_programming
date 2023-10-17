d64e1c95f1f4906fd7a72ad6487aeccf67d6501f
castykawira <murithicasty@gmail.com>
2023-10-17 06:33:07 -0700

    question 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Task: Create a table second_table and add multiple rows in the specified MySQL database
+-- SQL Query: Create the table if it doesn't exist
+
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
