04bc7c4f6ef0d496ab6f7992df094d7f2ed3a874
castykawira <murithicasty@gmail.com>
2023-10-17 05:54:47 -0700

    question 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Task: Create a table called first_table in the current MySQL database
+
+-- SQL Query: Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
